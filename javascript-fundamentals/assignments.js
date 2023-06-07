7dd38486234e99b19491db9058f499d19fbcc0a8
Alucia <analuciacarhuaz@gmail.com>
2023-06-07 16:22:04 -0500

    chore(): values and variables

---
+++ javascript-fundamentals/assignments.js
@@ -0,0 +1,12 @@
+// LECTURE: Values and Variables
+/*1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)*/
+
+let country = 'Peru';
+let continent = 'America';
+let population = 34312992;
+
+// 2. Log their values to the console
+console.log(country);
+console.log(continent);
+console.log(population);
